574a55f6f7725b86d4e7a46887c060d65404b6d7
pave-hammer <42391265+pave-hammer@users.noreply.github.com>
2019-04-04 18:42:27 -0600

    cool stuff happening

--- src/app/components/spells-list/spells-list.component.ts
+++ src/app/components/spells-list/spells-list.component.ts
@@ -8,10 +8,23 @@ import Spells from '../../../assets/spells.json';
 })
 export class SpellsListComponent implements OnInit {
   constructor() { this.allSpells = Spells }
-  ngOnInit() { this.getSpells() }
+  ngOnInit() { this.getSpells(), this.getAbilities() }
 
   allSpells: any;
   mySpells: Array<any> = [];
+  abilities: Array<Object> = []
+
+  getAbilities() {
+    const statArray: number[] = [8, 18, 10, 12, 15, 12]
+    return this.abilities = [
+      {name: "Str", stat: statArray[0], mod: Math.floor((statArray[0] - 10) / 2)},
+      {name: "Dex", stat: statArray[1], mod: Math.floor((statArray[1] - 10) / 2)},
+      {name: "Con", stat: statArray[2], mod: Math.floor((statArray[2] - 10) / 2)},
+      {name: "Int", stat: statArray[3], mod: Math.floor((statArray[3] - 10) / 2)},
+      {name: "Wis", stat: statArray[4], mod: Math.floor((statArray[4] - 10) / 2)},
+      {name: "Cha", stat: statArray[5], mod: Math.floor((statArray[5] - 10) / 2)}
+    ]
+  }
 
   getSpells() {
     const index: Array<number> = [163, 185, 79, 235, 209, 320]
